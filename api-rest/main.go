3cbdd9eaa2265d8bc03e0843de110ae3745be8d8
Sabrina Saczkowski <sabrina.sacz@gmail.com>
2018-07-18 00:18:34 -0300

    Added gorilla mux for routing

--- api-rest/main.go
+++ api-rest/main.go
@@ -1,17 +1,28 @@
 package main
 
-import(
+import (
 	"fmt"
-	"net/http"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w,"Hola mundo desde mi servidor web con Go")
-	})
+func main() {
 
-	server := http.ListenAndServe(":4000",nil)
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contact", Contact)
+
+	fmt.Println("El servidor está corriendo en http://localhost:3000.")
+	server := http.ListenAndServe(":3000", router)
 
 	log.Fatal(server)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo desde mi servidor web con Go.")
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Ésta es la página de contacto.")
 }
